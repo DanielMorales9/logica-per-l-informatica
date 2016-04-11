ffa2f83236fbdc4cc753e5f7613c8362bc763d86
Daniel <dnlmrls9@gmail.com>
2016-04-11 20:11:15 +0200

    split refactor

--- split.pl
+++ split.pl
@@ -1,5 +1,5 @@
 pari(X) :- 0 is X mod 2.
 
 split([], [], []).
-split([L|C1], [P|C2], C3) :- pari(L) ,!,  L=P, split(C1, C2, C3). 
-split([L|C1], C2, [D|C3]) :- not(pari(L)) ,!, L=D, split(C1, C2, C3).
+split([X|C1], [X|C2], C3) :- pari(X), !, split(C1, C2, C3). 
+split([X|C1], C2, [X|C3]) :- split(C1, C2, C3).
