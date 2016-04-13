4b1a10b371265f16ea20194ae0feda917ef28958
Daniel Morales <dnlmrls9@gmail.com>
2016-04-13 17:01:02 +0200

    set2014.pl

---
+++ set2014.pl
@@ -0,0 +1,7 @@
+applica_sost(F & Something,Sost, P & G) :- map(F, Sost, P),!, applica_sost(Something, Sost, G). applica_sost(F v Something,Sost,P v G) :- map(F, Sost, P),!, applica_sost(Something, Sost, G).
+applica_sost(F => Something,Sost,P => G) :- map(F, Sost, P),!,  applica_sost(Something, Sost, G).
+applica_sost(F, Sost, P):-map(F, Sost, P), !.
+
+map(F, [], F).
+map(F, [(F, P)|_], P) :- !.
+map(F, [_|Rest], P) :- map(F, Rest, P).
