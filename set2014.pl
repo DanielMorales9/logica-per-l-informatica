b66b88154218bf0180cede2877680ab94ecff71c
Daniel Morales <dnlmrls9@gmail.com>
2016-04-13 19:08:43 +0200

    set2014 fixed

--- set2014.pl
+++ set2014.pl
@@ -1,6 +1,6 @@
-applica_sost(F & Something,Sost, P & G) :- applica_sost(F, Sost, P), !, applica_sost(Something, Sost, G).
-applica_sost(F v Something,Sost,P v G) :- applica_sost(F, Sost, P), !, applica_sost(Something, Sost, G).
-applica_sost(F => Something,Sost,P => G) :- applica_sost(F, Sost, P), !, applica_sost(Something, Sost, G).
+applica_sost(F & Something,Sost, P & G) :- applica_sost(F, Sost, P),applica_sost(Something, Sost, G), !.
+applica_sost(F v Something,Sost,P v G) :- applica_sost(F, Sost, P), applica_sost(Something, Sost, G), !.
+applica_sost(F => Something,Sost,P => G) :- applica_sost(F, Sost, P), applica_sost(Something, Sost, G), !.
 applica_sost(F, Sost, P):- map(F, Sost, P), !.
 
 map(F, [], F).
