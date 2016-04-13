9f59313c3aff98df5deea2379a9cac7b1997e809
Daniel Morales <dnlmrls9@gmail.com>
2016-04-13 20:00:23 +0200

    piccola modifica

--- set2014.pl
+++ set2014.pl
@@ -1,8 +1,5 @@
 applica_sost(F & Something,Sost, P & G) :- applica_sost(F, Sost, P),applica_sost(Something, Sost, G), !.
 applica_sost(F v Something,Sost,P v G) :- applica_sost(F, Sost, P), applica_sost(Something, Sost, G), !.
 applica_sost(F => Something,Sost,P => G) :- applica_sost(F, Sost, P), applica_sost(Something, Sost, G), !.
-applica_sost(F, Sost, P):- map(F, Sost, P), !.
-
-map(F, [], F).
-map(F, [(F, P)|_], P) :- !.
-map(F, [_|Rest], P) :- map(F, Rest, P).
+applica_sost(F, Sost, P):- member((F, P), Sost), !.
+applica_sost(F, _, F).
